21abaf053d7bf17618801e41827596ff4f289f18
alfonzosharpe <71534280+alfonzosharpe@users.noreply.github.com>
2021-04-25 15:32:14 -0400

    Add files via upload

--- Repo/update.php
+++ Repo/update.php
@@ -9,9 +9,13 @@ $dbname="mydb";
 
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
     or die ('Could not connect to the database server' . mysqli_connect_error());
+	
+	
+	$page = mysqli_real_escape_string($con, $_REQUEST['page']);
+	
 if( $page=="genre"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search2']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search2']);
 	$sql = "UPDATE genre SET genre_name='$secsearch' WHERE genre_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -19,8 +23,8 @@ if( $page=="genre"){
 	header("location: update_DB.php");
 }
 elseif( $page=="studio"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search3']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search4']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search3']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search4']);
 	$sql = "UPDATE studio SET studio_name='$secsearch' WHERE studio_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -28,8 +32,8 @@ elseif( $page=="studio"){
 	header("location: update_DB.php");
 }
 elseif( $page=="director"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search5']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search6']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search5']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search6']);
 	$sql = "UPDATE director SET director_name='$secsearch' WHERE director_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -37,8 +41,8 @@ elseif( $page=="director"){
 	header("location: update_DB.php");
 }
 elseif( $page=="stream"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search7']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search8']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search7']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search8']);
 	$sql = "UPDATE stream SET stream_name='$secsearch' WHERE stream_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -46,8 +50,8 @@ elseif( $page=="stream"){
 	header("location: update_DB.php");
 }
 elseif( $page=="Name_ENG"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search9']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search10']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search9']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search10']);
 	$sql = "UPDATE anime SET Name_ENG='$secsearch' WHERE anime_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -56,8 +60,8 @@ elseif( $page=="Name_ENG"){
 }
 
 elseif( $page=="Name_JPN"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search11']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search12']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search11']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search12']);
 	$sql = "UPDATE anime SET Name_JPN='$secsearch' WHERE anime_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -65,8 +69,8 @@ elseif( $page=="Name_JPN"){
 	header("location: update_DB.php");
 }
 elseif( $page=="Maturity"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search13']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search14']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search13']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search14']);
 	$sql = "UPDATE anime SET Maturity='$secsearch' WHERE anime_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
@@ -74,8 +78,8 @@ elseif( $page=="Maturity"){
 	header("location: update_DB.php");
 }
 elseif( $page=="AirDate"){
-	$name = mysqli_real_escape_string($con, $_REQUEST['search15']));
-	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search16']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search15']);
+	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search16']);
 	$sql = "UPDATE anime SET AirDate='$secsearch' WHERE anime_id ='$name'";
 		if(mysqli_query($con, $sql)){
         		echo "Records added successfully.";
--- Repo/update_DB.php
+++ Repo/update_DB.php
@@ -17,10 +17,11 @@ genre {color: white;}
 </head>
 
 <body>
+
 <nav class = "horiz" >
 <ul>
-	<li style = "font-size: 34px"><a onclick="based()">The Anime Repo:</a></li>
-	<li style = "font-size: 34px"> Update Page</li>
+	<li style = "font-size: 34px"><a href="http://localhost/REPO/home_update.php">The Anime Repo</a></li>
+	<li><a href="http://localhost/REPO/update_DB.php">Update</a></li>
 </ul>
 </nav>
 
@@ -40,6 +41,7 @@ genre {color: white;}
             <br>
             <br>
             <label for = "anime_nameENG_update">Updated Anime English Name: </label>
+			<input type="hidden" name="page" value="Name_ENG">
             <input type="text" name="search10" id = "animeNameENG_update">
 		</p>
 		
@@ -52,10 +54,11 @@ genre {color: white;}
 			<input type="text" name="search11" id = "animeNameJAPN">
             <br>
             <br>
-            <label for = "anime_nameJAPN_update"> Updadted Anime Japanese Name: </label>
+            <label for = "anime_nameJAPN_update"> Updated Anime Japanese Name: </label>
+			
             <input type="text" name="search12" id = "animeNameJAPN_update">
 		</p>
-		
+		<input type="hidden" name="page" value="Name_JPN">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -65,10 +68,11 @@ genre {color: white;}
 			<input type="text" name="search5" id = "animeDirector">
             <br>
             <br>
-            <label for = "anime_director_update"> Updadted Director Name: </label>
+            <label for = "anime_director_update"> Updated Director Name: </label>
+			
             <input type="text" name="search6" id = "animeDirector_update">
 		</p>
-		
+		<input type="hidden" name="page" value="director">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -78,11 +82,12 @@ genre {color: white;}
 			<input type="text" name="search3" id = "animeStudio">
             <br>
             <br>
-            <label for = "anime_studio_update"> Updadted Studio Name: </label>
+            <label for = "anime_studio_update"> Updated Studio Name: </label>
+			
             <input type="text" name="search4" id = "animeStudio_update">
 		
 		</p>
-		
+		<input type="hidden" name="page" value="studio">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -92,10 +97,11 @@ genre {color: white;}
 			<input type="text" name="search7" id = "animeStream">
             <br>
             <br>
-            <label for = "anime_stream_update"> Updadted Streaming Service: </label>
+            <label for = "anime_stream_update"> Updated Streaming Service: </label>
+			
             <input type="text" name="search8" id = "animeStream_update">
 		</p>
-		
+		<input type="hidden" name="page" value="stream">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -105,10 +111,11 @@ genre {color: white;}
 			<input type="text" name="search1" id = "animeStream">
             <br>
             <br>
-            <label for = "anime_genre_update"> Updadted Genre Style: </label>
+            <label for = "anime_genre_update"> Updated Genre Style: </label>
+
             <input type="text" name="search2" id = "animeGenre_update">
 		</p>
-		
+		<input type="hidden" name="page" value="genre">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -118,10 +125,11 @@ genre {color: white;}
 			<input type="text" name="search13" id = "animeMaturity">
             <br>
             <br>
-            <label for = "anime_maturity_update"> Updadted Maturity Rating: </label>
+            <label for = "anime_maturity_update"> Updated Maturity Rating: </label>
+
             <input type="text" name="search14" id = "animeMaturity_update">
 		</p>
-		
+		<input type="hidden" name="page" value="Maturity">
 		<input type="submit" value="Submit">
 	</form>
      <hr>
@@ -131,10 +139,10 @@ genre {color: white;}
 			<input type="text" name="search15" id = "animeAirDate">
             <br>
             <br>
-            <label for = "anime_airDate_update"> Updadted Air Date: </label>
+            <label for = "anime_airDate_update"> Updated Air Date: </label>
             <input type="text" name="search16" id = "animeAirDate_update">
 		</p>
-		
+		<input type="hidden" name="page" value="AirDate">
 		<input type="submit" value="Submit">
 	</form>
      
