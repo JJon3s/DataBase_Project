66e423b1ab1fe724b725f64acb5dfe90a8158ede
Camron Grant <56035567+camrongrant1@users.noreply.github.com>
2021-04-25 14:48:35 -0400

    Update update.php

--- Repo/update.php
+++ Repo/update.php
@@ -10,62 +10,77 @@ $dbname="mydb";
 $con = new mysqli($host, $user, $password, $dbname, $port, $socket)
     or die ('Could not connect to the database server' . mysqli_connect_error());
 if( $page=="genre"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search']));
+	$name = mysqli_real_escape_string($con, $_REQUEST['search']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search2']));
-    $sql = "UPDATE genre SET genre_name='$secsearch' WHERE genre_id ='$name'";
+	$sql = "UPDATE genre SET genre_name='$secsearch' WHERE genre_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-if( $page=="studio"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search3']));
+elseif( $page=="studio"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search3']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search4']));
-    $sql = "UPDATE studio SET studio_name='$secsearch' WHERE studio_id ='$name'";
+	$sql = "UPDATE studio SET studio_name='$secsearch' WHERE studio_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-if( $page=="director"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search5']));
+elseif( $page=="director"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search5']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search6']));
-    $sql = "UPDATE director SET director_name='$secsearch' WHERE director_id ='$name'";
+	$sql = "UPDATE director SET director_name='$secsearch' WHERE director_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-
-if( $page=="stream"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search7']));
+elseif( $page=="stream"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search7']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search8']));
-    $sql = "UPDATE stream SET stream_name='$secsearch' WHERE stream_id ='$name'";
+	$sql = "UPDATE stream SET stream_name='$secsearch' WHERE stream_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-if( $page=="Name_ENG"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search9']));
+elseif( $page=="Name_ENG"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search9']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search10']));
-    $sql = "UPDATE anime SET Name_ENG='$secsearch' WHERE anime_id ='$name'";
+	$sql = "UPDATE anime SET Name_ENG='$secsearch' WHERE anime_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
 
-if( $page=="Name_JPN"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search11']));
+elseif( $page=="Name_JPN"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search11']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search12']));
-    $sql = "UPDATE anime SET Name_JPN='$secsearch' WHERE anime_id ='$name'";
+	$sql = "UPDATE anime SET Name_JPN='$secsearch' WHERE anime_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-if( $page=="Maturity"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search13']));
+elseif( $page=="Maturity"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search13']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search14']));
-    $sql = "UPDATE anime SET Maturity='$secsearch' WHERE anime_id ='$name'";
+	$sql = "UPDATE anime SET Maturity='$secsearch' WHERE anime_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
-if( $page=="AirDate"){
-    $name = mysqli_real_escape_string($con, $_REQUEST['search15']));
+elseif( $page=="AirDate"){
+	$name = mysqli_real_escape_string($con, $_REQUEST['search15']));
 	$secsearch = mysqli_real_escape_string($con, $_REQUEST['search16']));
-    $sql = "UPDATE anime SET AirDate='$secsearch' WHERE anime_id ='$name'";
+	$sql = "UPDATE anime SET AirDate='$secsearch' WHERE anime_id ='$name'";
+		if(mysqli_query($con, $sql)){
+        		echo "Records added successfully.";
+    		}
 	header("location: update.php");
-
 }
 // Close connection
 mysqli_close($con);
